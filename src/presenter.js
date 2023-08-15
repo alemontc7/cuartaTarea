baa16051163cc49be3a7690dac80444e21224e0a
Alejandro Montero Coca <alejandro.montero@ucb.edu.bo>
2023-08-15 08:20:19 -0400

    commit

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,17 @@
 import sumar from "./sumador";
+import multiplicar from "./mutltiplicador ";
+
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const firstm = document.querySelector("#primer-numero-m");
+const secondm = document.querySelector("#segundo-numero-m");
+const formm = document.querySelector("#multiplicar-form");
+const divm = document.querySelector("#resultado-mult");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,3 +21,14 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
+
+formm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstm.value);
+  const secondNumber = Number.parseInt(secondm.value);
+
+  divm.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+});
+
+
